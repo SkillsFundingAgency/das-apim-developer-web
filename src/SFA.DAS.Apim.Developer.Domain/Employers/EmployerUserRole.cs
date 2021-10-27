47220daf1408fcc839d693dde74978e8584fba3b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-10-27 14:48:24 +0100

    FAI-126 - Add EmployerAccountService for getting user accounts and roles

---
+++ src/SFA.DAS.Apim.Developer.Domain/Employers/EmployerUserRole.cs
@@ -0,0 +1,10 @@
+namespace SFA.DAS.Apim.Developer.Domain.Employers
+{
+    public enum EmployerUserRole
+    {
+        None = 0,
+        Owner = 1,
+        Transactor = 2,
+        Viewer = 3
+    }
+}
