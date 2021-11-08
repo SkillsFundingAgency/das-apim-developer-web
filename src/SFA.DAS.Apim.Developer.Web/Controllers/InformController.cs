0c190f8c9d08626f3b6b45f8128307f4fda9e780
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-08 14:20:41 +0000

    FAI-128 - Correct navigation setting

--- src/SFA.DAS.Apim.Developer.Web/Controllers/InformController.cs
+++ src/SFA.DAS.Apim.Developer.Web/Controllers/InformController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Apim.Developer.Web.Infrastructure;
 using SFA.DAS.Apim.Developer.Web.Infrastructure.Configuration;
+using SFA.DAS.Provider.Shared.UI;
+using SFA.DAS.Provider.Shared.UI.Attributes;
 
 namespace SFA.DAS.Apim.Developer.Web.Controllers
 {
@@ -27,6 +29,7 @@ namespace SFA.DAS.Apim.Developer.Web.Controllers
         [HttpGet]
         [Authorize(Policy = nameof(PolicyNames.HasProviderAccount))]
         [Route("{ukprn}/recruitment/api", Name = RouteNames.ProviderRecruitInform)]
+        [SetNavigationSection(NavigationSection.Recruit)]
         public IActionResult ProviderIndex()
         {
             return View();
