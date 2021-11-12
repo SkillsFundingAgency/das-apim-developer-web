6058e07375117864c8c913d5baa4f0bc7ae1e0b2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-12 21:00:43 +0000

    FAI-140 - Add tag helper for building api description with link

---
+++ src/SFA.DAS.Apim.Developer.Web/TagHelpers/ApiDescriptionTagHelper.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+
+namespace SFA.DAS.Apim.Developer.Web.TagHelpers
+{
+    [HtmlTargetElement("api-description")]
+    public class ApiDescriptionTagHelper : TagHelper
+    {
+        private readonly IApiDescriptionHelper _apiDescriptionHelper;
+
+        public ApiDescriptionTagHelper (IApiDescriptionHelper apiDescriptionHelper)
+        {
+            _apiDescriptionHelper = apiDescriptionHelper;
+        }
+        public string Data { get; set; }
+        public override void Process(TagHelperContext tagHelperContext, TagHelperOutput tagHelperOutput)
+        {
+            
+            tagHelperOutput.TagName = "p";
+            tagHelperOutput.AddClass("govuk-body",HtmlEncoder.Default);
+            tagHelperOutput.Content.SetHtmlContent(_apiDescriptionHelper.ProcessApiDescription(Data));
+            tagHelperOutput.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
