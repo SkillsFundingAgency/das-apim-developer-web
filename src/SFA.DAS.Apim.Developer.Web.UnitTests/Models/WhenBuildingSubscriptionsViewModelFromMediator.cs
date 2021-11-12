53d323a646d630787e22e0d747ba35632ffb2669
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-12 13:56:37 +0000

    FAI-140 - Moved view and added viewmodel to api hubs

---
+++ src/SFA.DAS.Apim.Developer.Web.UnitTests/Models/WhenBuildingSubscriptionsViewModelFromMediator.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Application.Subscriptions.Queries.GetAvailableProducts;
+using SFA.DAS.Apim.Developer.Web.Models;
+
+namespace SFA.DAS.Apim.Developer.Web.UnitTests.Models
+{
+    public class WhenBuildingSubscriptionsViewModelFromMediator
+    {
+        [Test, AutoData]
+        public void Then_The_Model_Is_Built(GetAvailableProductsQueryResult source)
+        {
+            var actual = (SubscriptionsViewModel)source;
+
+            
+            actual.Products.Select(x=>x.DisplayName).Should().BeEquivalentTo(source.Products.Products.Select(c=>c.DisplayName));
+            actual.Products.Select(x=>x.Description).Should().BeEquivalentTo(source.Products.Products.Select(c=>c.Description));
+            actual.Products.Select(x=>x.Name).Should().BeEquivalentTo(source.Products.Products.Select(c=>c.Name.ToLower()));
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Web/Models/SubscriptionsViewModel.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.Apim.Developer.Application.Subscriptions.Queries.GetAvailableProducts;
+using SFA.DAS.Apim.Developer.Domain.Subscriptions;
+
+namespace SFA.DAS.Apim.Developer.Web.Models
+{
+    public class SubscriptionsViewModel
+    {
+        public List<SubscriptionItem> Products { get; set; }
+        public static implicit operator SubscriptionsViewModel(GetAvailableProductsQueryResult source)
+        {
+            return new SubscriptionsViewModel
+            {
+                Products = source.Products.Products.Select(c=>(SubscriptionItem)c).ToList()
+
+            };
+        }
+    }
+
+    public class SubscriptionItem
+    {
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public static implicit operator SubscriptionItem(ProductSubscriptionItem source)
+        {
+            return new SubscriptionItem
+            {
+                DisplayName = source.DisplayName,
+                Name = source.Name.ToLower(),
+                Description = source.Description
+            };
+        }
+    }
+}
