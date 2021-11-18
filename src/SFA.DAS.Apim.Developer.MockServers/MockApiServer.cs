ecb07b7b8e52fce2cc927b020f9ca54a717a0f7d
A-J-Wright <alissa.wright@education.gov.uk>
2021-11-18 16:16:31 +0000

    create mockserver project and connect port

---
+++ src/SFA.DAS.Apim.Developer.MockServers/MockApiServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WireMock.Logging;
+using WireMock.Matchers;
+using WireMock.Net.StandAlone;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+using WireMock.Types;
+
+namespace SFA.DAS.Apim.Developer.MockServer
+{
+    public static class MockApiServer
+    {
+        public static IWireMockServer Start()
+        {
+            var settings = new WireMockServerSettings
+            {
+                Port = 5031,
+                Logger = new WireMockConsoleLogger()
+            };
+            var server = StandAloneApp.Start(settings);
+
+            return server;
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.MockServers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SFA.DAS.Apim.Developer.MockServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mock Server starting on http://localhost:5010");
+
+            MockApiServer.Start();
+
+            Console.WriteLine(("Press any key to stop the server"));
+            Console.ReadKey();
+        }
+    }
+}
