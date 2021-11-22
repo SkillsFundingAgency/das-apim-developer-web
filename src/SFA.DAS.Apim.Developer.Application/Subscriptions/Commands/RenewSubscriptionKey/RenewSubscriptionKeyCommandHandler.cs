2555777d45caa91fd136dafac3c55fdd2916855f
Scott Wakefield <scott@expertcc.co.uk>
2021-11-22 12:12:34 +0000

    FAI-160 add handler.

---
+++ src/SFA.DAS.Apim.Developer.Application/Subscriptions/Commands/RenewSubscriptionKey/RenewSubscriptionKeyCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+using SFA.DAS.Apim.Developer.Domain.Subscriptions.Api.Requests;
+
+namespace SFA.DAS.Apim.Developer.Application.Subscriptions.Commands.RenewSubscriptionKey
+{
+    public class RenewSubscriptionKeyCommandHandler : IRequestHandler<RenewSubscriptionKeyCommand, Unit>
+    {
+        private readonly IApiClient _apiClient;
+
+        public RenewSubscriptionKeyCommandHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        
+        public async Task<Unit> Handle(RenewSubscriptionKeyCommand request, CancellationToken cancellationToken)
+        {
+            var apiRequest = new PostRenewSubscriptionKeyRequest(request.AccountIdentifier, request.ProductId);
+            var apiResponse = await _apiClient.Post<string>(apiRequest);
+            //todo: should check if successful or not in future story
+            return Unit.Value;
+        }
+    }
+}
