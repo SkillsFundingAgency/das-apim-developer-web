b0412a61c2e3e93d28c19d4d19572c5daa0dfdfb
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-24 07:21:30 +0000

    FAI-141 - Update view with new styling

--- src/SFA.DAS.Apim.Developer.Web/Infrastructure/RouteNames.cs
+++ src/SFA.DAS.Apim.Developer.Web/Infrastructure/RouteNames.cs
@@ -6,6 +6,8 @@ namespace SFA.DAS.Apim.Developer.Web.Infrastructure
         public const string EmployerApiHub = "employer-api-hub";
         public const string EmployerRenewKey = "employer-renew-key";
         public const string EmployerKeyRenewed = "employer-key-renewed";
+        public const string EmployerGetKey = "employer-key-get";
+        public const string EmployerViewKey = "employer-view-get";
 
         public const string ProviderRecruitInform = "provider-recruit-inform";
         public const string RecruitDocumentation = "recruitment_api_doc";
