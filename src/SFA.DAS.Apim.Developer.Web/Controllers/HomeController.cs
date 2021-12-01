ae4a02959e340d85dc9826756267b4d76b0454d7
A-J-Wright <alissa.wright@education.gov.uk>
2021-12-01 14:08:06 +0000

    created home controller

---
+++ src/SFA.DAS.Apim.Developer.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Apim.Developer.Web.Infrastructure;
+
+namespace SFA.DAS.Apim.Developer.Web.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("index", Name = RouteNames.Index)]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
