467bb77793c66a4097fdafcc10dd9951370ba80e
Scott Wakefield <scott@expertcc.co.uk>
2021-12-01 16:20:47 +0000

    FAI-166 adding cast operator.

---
+++ src/SFA.DAS.Apim.Developer.Web.UnitTests/Models/WhenBuildingRegisterViewModelFromRegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Web.Models.ThirdPartyAccounts;
+
+namespace SFA.DAS.Apim.Developer.Web.UnitTests.Models
+{
+    public class WhenBuildingRegisterViewModelFromRegisterRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Model_Is_Built(RegisterRequest source)
+        {
+            var actual = (RegisterViewModel)source;
+
+            actual.Should().BeEquivalentTo(source);
+        }
+    }
+}
--- src/SFA.DAS.Apim.Developer.Web/Models/ThirdPartyAccounts/RegisterViewModel.cs
+++ src/SFA.DAS.Apim.Developer.Web/Models/ThirdPartyAccounts/RegisterViewModel.cs
@@ -7,5 +7,17 @@
         public string EmailAddress { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+
+        public static implicit operator RegisterViewModel(RegisterRequest source)
+        {
+            return new RegisterViewModel
+            {
+                FirstName = source.FirstName,
+                LastName = source.LastName,
+                EmailAddress = source.EmailAddress,
+                Password = source.Password,
+                ConfirmPassword = source.ConfirmPassword
+            };
+        }
     }
 }
