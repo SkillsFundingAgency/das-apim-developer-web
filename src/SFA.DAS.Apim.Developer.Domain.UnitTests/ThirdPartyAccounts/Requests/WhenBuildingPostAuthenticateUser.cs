6a78da5d304e6632fb8c4dab9495b897c27062eb
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-12-09 09:33:41 +0000

    FAI-152 - Add authenticate request

---
+++ src/SFA.DAS.Apim.Developer.Domain.UnitTests/ThirdPartyAccounts/Requests/WhenBuildingPostAuthenticateUser.cs
@@ -0,0 +1,20 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests;
+
+namespace SFA.DAS.Apim.Developer.Domain.UnitTests.ThirdPartyAccounts.Requests
+{
+    public class WhenBuildingPostAuthenticateUser
+    {
+        [Test, AutoData]
+        public void Then_The_Url_And_Data_Is_Correctly_Set(string email, string password)
+        {
+            var actual = new PostAuthenticateUserRequest(email, password);
+
+            actual.PostUrl.Should().Be("api/users/authenticate");
+            ((PostAuthenticateUserRequestData)actual.Data).Email.Should().Be(email);
+            ((PostAuthenticateUserRequestData)actual.Data).Password.Should().Be(password);
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Domain/ThirdPartyAccounts/Api/Requests/PostAuthenticateUserRequest.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+
+namespace SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests
+{
+    public class PostAuthenticateUserRequest : IPostApiRequest
+    {
+        public PostAuthenticateUserRequest (string email, string password)
+        {
+            Data = new PostAuthenticateUserRequestData
+            {
+                Email = email,
+                Password = password
+            };
+        }
+
+        public string PostUrl => "api/users/authenticate";
+        public object Data { get; set; }
+
+    }
+
+    public class PostAuthenticateUserRequestData
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
