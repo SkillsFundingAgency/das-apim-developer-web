db10f225581996c336aeffb50082835841441cc9
Scott Wakefield <scott@expertcc.co.uk>
2021-12-13 18:02:04 +0000

    FAI-169 add handler.

---
+++ src/SFA.DAS.Apim.Developer.Application/ThirdPartyAccounts/Commands/VerifyRegistration/VerifyRegistrationCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace SFA.DAS.Apim.Developer.Application.ThirdPartyAccounts.Commands.VerifyRegistration
+{
+    public class VerifyRegistrationCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Application/ThirdPartyAccounts/Commands/VerifyRegistration/VerifyRegistrationCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests;
+
+namespace SFA.DAS.Apim.Developer.Application.ThirdPartyAccounts.Commands.VerifyRegistration
+{
+    public class VerifyRegistrationCommandHandler : IRequestHandler<VerifyRegistrationCommand, Unit>
+    {
+        private readonly IApiClient _apiClient;
+
+        public VerifyRegistrationCommandHandler(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        
+        public async Task<Unit> Handle(VerifyRegistrationCommand request, CancellationToken cancellationToken)
+        {
+            var apiRequest = new PutActivateThirdPartyUserRequest(request.Id);
+            var apiResponse = await _apiClient.Put<string>(apiRequest);
+
+            if (apiResponse.StatusCode != HttpStatusCode.NoContent)
+            {
+                throw new InvalidOperationException(apiResponse.ErrorContent);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
