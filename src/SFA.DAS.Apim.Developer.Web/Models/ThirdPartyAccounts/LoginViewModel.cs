1ef2e6edf2bd01cfb82e954c03dd4436133f3c5b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-12-13 17:29:05 +0000

    FAI-152 - Add order to errors displayed

--- src/SFA.DAS.Apim.Developer.Web/Models/ThirdPartyAccounts/LoginViewModel.cs
+++ src/SFA.DAS.Apim.Developer.Web/Models/ThirdPartyAccounts/LoginViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace SFA.DAS.Apim.Developer.Web.Models.ThirdPartyAccounts
 {
@@ -8,5 +10,16 @@ namespace SFA.DAS.Apim.Developer.Web.Models.ThirdPartyAccounts
         public string EmailAddress { get; set; }
         [Required(ErrorMessage = "Please enter your password")]
         public string Password { get; set; }
+        
+        public static Dictionary<string, int> BuildPropertyOrderDictionary()
+        {
+            var itemCount = 0;
+            var propertyOrderDictionary = typeof(LoginViewModel).GetProperties().Select(c => new
+            {
+                Order = itemCount++,
+                c.Name
+            }).ToDictionary(key => key.Name, value => value.Order);
+            return propertyOrderDictionary;
+        }
     }
 }
