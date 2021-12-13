dbefe1949bf892ee9785c918c34e780c8083496d
Scott Wakefield <scott@expertcc.co.uk>
2021-12-13 17:51:47 +0000

    FAI-169 adding put request.

--- src/SFA.DAS.Apim.Developer.Domain/ThirdPartyAccounts/Api/Requests/PutActivateThirdPartyUserRequest.cs
+++ src/SFA.DAS.Apim.Developer.Domain/ThirdPartyAccounts/Api/Requests/PutActivateThirdPartyUserRequest.cs
@@ -1,10 +1,18 @@
-﻿using SFA.DAS.Apim.Developer.Domain.Interfaces;
+﻿using System;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
 
 namespace SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests
 {
     public class PutActivateThirdPartyUserRequest : IPutApiRequest
     {
-        public string PutUrl { get; }
+        private readonly Guid _id;
+
+        public PutActivateThirdPartyUserRequest(Guid Id)
+        {
+            _id = Id;
+        }
+
+        public string PutUrl => $"users/{_id}/activate";
         public object Data { get; set; }
     }
 }
