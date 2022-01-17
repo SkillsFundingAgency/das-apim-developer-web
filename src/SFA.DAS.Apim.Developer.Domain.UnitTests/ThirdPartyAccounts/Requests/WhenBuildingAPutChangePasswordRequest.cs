0db7f6d6209f26c8093ee8a480990026c278ff79
Scott Wakefield <scott@expertcc.co.uk>
2022-01-17 19:57:31 +0000

    FAI-193 adding change password api request.

---
+++ src/SFA.DAS.Apim.Developer.Domain.UnitTests/ThirdPartyAccounts/Requests/WhenBuildingAPutChangePasswordRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests;
+
+namespace SFA.DAS.Apim.Developer.Domain.UnitTests.ThirdPartyAccounts.Requests
+{
+    public class WhenBuildingAPutChangePasswordRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed(
+            Guid id,
+            string password)
+        {
+            //Act
+            var actual = new PutChangePasswordRequest(id, password);
+            
+            //Assert
+            actual.PutUrl.Should().Be($"users/{id}/change-password");
+            ((PutChangePasswordRequestData)actual.Data).Password.Should().BeEquivalentTo(password);
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Domain/ThirdPartyAccounts/Api/Requests/PutChangePasswordRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFA.DAS.Apim.Developer.Domain.Interfaces;
+
+namespace SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Requests
+{
+    public class PutChangePasswordRequest : IPutApiRequest
+    {
+        private readonly Guid _id;
+
+        public PutChangePasswordRequest(Guid id, string password)
+        {
+            _id = id;
+            Data = new PutChangePasswordRequestData(password);
+        }
+
+        public string PutUrl => $"users/{_id}/change-password";
+        public object Data { get; set; }
+    }
+
+    public class PutChangePasswordRequestData
+    {
+        public PutChangePasswordRequestData(string password)
+        {
+            Password = password;
+        }
+
+        public string Password { get; set; }
+    }
+}
