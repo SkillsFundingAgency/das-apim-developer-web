54a81a91211505e7354791fabbca426b50446e70
Scott Wakefield <scott@expertcc.co.uk>
2022-01-18 10:29:00 +0000

    FAI-193 adding new user details domain type.

---
+++ src/SFA.DAS.Apim.Developer.Domain.UnitTests/ThirdPartyAccounts/WhenMappingToAuthenticateUserDetailsFromPostAuthenticateUserResponse.cs
@@ -0,0 +1,21 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts;
+using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Responses;
+
+namespace SFA.DAS.Apim.Developer.Domain.UnitTests.ThirdPartyAccounts
+{
+    public class WhenMappingToAuthenticateUserDetailsFromPostAuthenticateUserResponse
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(PostAuthenticateUserResponseItem source)
+        {
+            //Act
+            var actual = (AuthenticateUserDetails)source;
+            
+            //Assert
+            actual.Should().BeEquivalentTo(source);
+        }
+    }
+}
---
+++ src/SFA.DAS.Apim.Developer.Domain/ThirdPartyAccounts/UserDetails.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts.Api.Responses;
+
+namespace SFA.DAS.Apim.Developer.Domain.ThirdPartyAccounts
+{
+    public class UserDetails
+    {
+        public static implicit operator UserDetails(GetUserResponseItem source)
+        {
+            return new UserDetails
+            {
+                Id = source.Id,
+                Email = source.Email,
+                State = source.State,
+                FirstName = source.FirstName,
+                LastName = source.LastName
+            };
+        }
+        
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string State { get; set; }
+    }
+}
