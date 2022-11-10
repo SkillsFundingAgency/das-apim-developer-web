14caf37a51c23544da6140592aa15255103cf8fc
Alasdair Reid <ali.reid@live.co.uk>
2022-11-10 14:00:19 +0000

    Signout functionality

--- src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
+++ src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
@@ -1,21 +1,47 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Apim.Developer.Domain.Configuration;
+using SFA.DAS.Apim.Developer.Web.AppStart;
 using SFA.DAS.Apim.Developer.Web.Infrastructure;
+using SFA.DAS.Apim.Developer.Web.Infrastructure.Configuration;
+using SFA.DAS.Provider.Shared.UI.Models;
 
 namespace SFA.DAS.Apim.Developer.Web.Controllers
 {
     public class AccountController : ControllerBase
     {
-        [Route("signout",Name = RouteNames.ProviderSignOut)]
-        public IActionResult ProviderSignOut()
+        private readonly IOptions<ApimDeveloperWeb> _apimDeveloperWebConfiguration;
+
+        public AccountController(IOptions<ApimDeveloperWeb> apimDeveloperWebConfiguration)
         {
+            _apimDeveloperWebConfiguration = apimDeveloperWebConfiguration;
+        }
+
+        [Route("signout", Name = RouteNames.ProviderSignOut)]
+        public async Task<IActionResult> ProviderSignOutAsync()
+        {
+            if (_apimDeveloperWebConfiguration.Value.UseDfESignIn.Equals(true))
+            {
+                var idToken = await HttpContext.GetTokenAsync("id_token");
+
+                var authenticationProperties = new AuthenticationProperties();
+                authenticationProperties.Parameters.Clear();
+                authenticationProperties.Parameters.Add("id_token", idToken);
+                return SignOut(
+                    authenticationProperties, CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme);
+            }
+
             return SignOut(
-                new Microsoft.AspNetCore.Authentication.AuthenticationProperties
-                {
-                    RedirectUri = "",
-                    AllowRefresh = true
-                },CookieAuthenticationDefaults.AuthenticationScheme, WsFederationDefaults.AuthenticationScheme);
+            new Microsoft.AspNetCore.Authentication.AuthenticationProperties
+            {
+                RedirectUri = "",
+                AllowRefresh = true
+            }, CookieAuthenticationDefaults.AuthenticationScheme, WsFederationDefaults.AuthenticationScheme);
+            
         }
     }
 }
