e064d5f981ccb499f43c47daaa2ed399aae1ff98
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-07-20 10:33:24 +0100

    Commit Details: 1. Added DfESignIn Nuget PAckage to the Web Project. 2. Integrated DfESignIn OpenIdConnect for the Provider Route. 3. Add the SFA.DAS.Provider.DfeSignIn config to the template & AppSettings.json file. 4. Add the AuthScheme for the SignOut Route
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-890

--- src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
+++ src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authentication.WsFederation;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Apim.Developer.Web.Infrastructure;
@@ -7,15 +8,31 @@ namespace SFA.DAS.Apim.Developer.Web.Controllers
 {
     public class AccountController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         [Route("signout",Name = RouteNames.ProviderSignOut)]
         public IActionResult ProviderSignOut()
         {
+            var useDfESignIn = _configuration["ApimDeveloperWeb:UseDfESignIn"] != null &&
+                               _configuration["ApimDeveloperWeb:UseDfESignIn"]
+                                   .Equals("true", StringComparison.CurrentCultureIgnoreCase);
+
+            var authScheme = useDfESignIn
+                ? OpenIdConnectDefaults.AuthenticationScheme
+                : WsFederationDefaults.AuthenticationScheme;
+
             return SignOut(
                 new Microsoft.AspNetCore.Authentication.AuthenticationProperties
                 {
                     RedirectUri = "",
                     AllowRefresh = true
-                },CookieAuthenticationDefaults.AuthenticationScheme, WsFederationDefaults.AuthenticationScheme);
+                },CookieAuthenticationDefaults.AuthenticationScheme, authScheme);
         }
     }
 }
