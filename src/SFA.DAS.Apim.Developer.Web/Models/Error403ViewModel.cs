80eca64ade927d0569b7856df890a559506d9bfe
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-08-29 11:51:59 +0100

    Commit Details: 403 Forbidden Error Page with content relevant to DfESignIn
    https://skillsfundingagency.atlassian.net/browse/FAI-906

---
+++ src/SFA.DAS.Apim.Developer.Web/Models/Error403ViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SFA.DAS.Apim.Developer.Web.Models
+{
+    public class Error403ViewModel
+    {
+        private readonly string _integrationUrlPart = string.Empty;
+
+        public Error403ViewModel(string environment)
+        {
+            if (!environment.Equals("prd", StringComparison.CurrentCultureIgnoreCase))
+            {
+                _integrationUrlPart = "test-";
+            }
+        }
+
+        public bool UseDfESignIn { get; set; }
+        public string HelpPageLink => $"https://{_integrationUrlPart}services.signin.education.gov.uk/organisations";
+        public string DashboardUrl { get; set; }
+    }
+}
