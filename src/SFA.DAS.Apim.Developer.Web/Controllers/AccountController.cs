a748302d4cbbec3418687024aa186af875a88e3b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-08-29 22:24:35 +0100

    FAI-886 - Sign out bug fix

--- src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
+++ src/SFA.DAS.Apim.Developer.Web/Controllers/AccountController.cs
@@ -27,7 +27,7 @@ namespace SFA.DAS.Apim.Developer.Web.Controllers
             return SignOut(
                 new Microsoft.AspNetCore.Authentication.AuthenticationProperties
                 {
-                    RedirectUri = "",
+                    //RedirectUri = "",
                     AllowRefresh = true
                 },CookieAuthenticationDefaults.AuthenticationScheme, authScheme);
         }
